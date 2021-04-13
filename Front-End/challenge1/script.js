0fc5909656c071c6b533f90f1cdb5ab5c24df4ab
eriverotupac <79490900+eriverotupac@users.noreply.github.com>
2021-04-13 00:57:31 -0500

    Challenge 1
    Finish challenge 1, modify counter.

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,17 @@
-// Modify this file only
+function modifyCount(event) {
+    const span = document.querySelector('span');
+    if (event.target.action == 'increase') {
+        span.textContent = parseInt(span.textContent) + 1;
+    }
+    if (event.target.action == 'decrease')
+        span.textContent = parseInt(span.textContent) - 1;
+}
+
+const btnIncrease = document.getElementById('increase');
+btnIncrease.action = 'increase';
+btnIncrease.addEventListener('click', modifyCount);
+
+
+const btnDecrease = document.getElementById('decrease');
+btnDecrease.action = 'decrease';
+btnDecrease.addEventListener('click', modifyCount);
