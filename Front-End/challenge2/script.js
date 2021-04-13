b7f86ed8dfbbdece3e080c33eab35d6fca8eafd6
Fran2304 <lilatiravanttiarteaga@gmail.com>
2021-04-13 10:34:00 -0500

    Calculator exercise

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable default-case */
 /*
 TO-DO:
 
@@ -5,3 +6,53 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+const allButtons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+display.textContent = '';
+
+let operatorA; let operatorB; let operation;
+
+function clean() {
+	display.textContent = '';
+}
+
+function reset() {
+	display.textContent = '';
+	operatorA = 0;
+	operatorB = 0;
+	operation = 0;
+}
+
+function resolve() {
+	let res = 0;
+	switch (operation) {
+	case 'add':
+		res = parseFloat(operatorA) + parseFloat(operatorB);
+		break;
+	case 'subtrack':
+		res = parseFloat(operatorA) - parseFloat(operatorB);
+		break;
+	case 'multiplication':
+		res = parseFloat(operatorA) * parseFloat(operatorB);
+		break;
+	case 'division':
+		res = parseFloat(operatorA) / parseFloat(operatorB);
+		break;
+	}
+	reset();
+	display.textContent = res;
+}
+allButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		if (Number.isInteger(parseInt(button.textContent, 10))) {
+			display.textContent += button.textContent;
+		} else if (button.id !== 'equals') {
+			operatorA = display.textContent;
+			operation = button.id;
+			clean();
+		} else {
+			operatorB = display.textContent;
+			resolve();
+		}
+	});
+});
