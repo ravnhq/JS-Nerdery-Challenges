b1e5e267edb510eaf208fdb6f9e57f9d02ebb606
Fran2304 <lilatiravanttiarteaga@gmail.com>
2021-04-12 19:08:30 -0500

    finish challenge 2

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,18 @@
 // Modify this file only
+const increase = document.querySelector('#increase');
+const decrease = document.querySelector('#decrease');
+const counter = document.querySelector('#counter');
+
+const sume = () => {
+	const history = parseInt(counter.textContent, 10);
+	counter.textContent = history + 1;
+};
+
+const rest = () => {
+	const history = (parseInt(counter.textContent, 10));
+	const result = history > 0 ? (history - 1) : 0;
+	counter.textContent = result;
+};
+
+increase.addEventListener('click', sume);
+decrease.addEventListener('click', rest);
