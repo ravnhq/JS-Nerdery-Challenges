a776441176216e1596260702af3c22461047e360
Abel AR <abel_john_14@hotmail.com>
2021-10-13 18:49:33 -0500

    Front-End/challenge2 show the numbers in the screen and create global variables

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,3 +5,33 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+
+
+const display = document.getElementById('display');
+const equals = document.getElementById('equals');
+let reference = "0"
+
+const options = function(option) {
+    return document.getElementById(`${option}`)
+}
+
+const numbers = function(number) {
+    return document.getElementById(`${number}`)
+}
+
+function addValue(num = '0') {
+    if((eval(reference) === 0 && (reference.indexOf(".") === -1))) {
+        reference = num
+    } else {
+        reference += num
+    }
+    
+    display.innerHTML = reference;
+}
+
+
+function init () {
+    addValue()
+}
+
+init();
