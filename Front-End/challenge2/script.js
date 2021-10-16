1dbb72a9f94fb719bb8df84935eac486bbdfe5c8
Marcelo Valdivia <marceloevv08@gmail.com>
2021-10-15 21:07:29 -0500

    Forgot to make a push (FE Challenge 2 completed, next step is include negative numbers)

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,49 +5,64 @@ TO-DO:
 - The calculator should be completely functional
 
 */
-
 let result = document.getElementById("display");
+var resultValue, oldValue, currentValue = 0;
+var action;
 
-const zeroButton = document.getElementById("zero");
-const oneButton = document.getElementById("one");
-const twoButton = document.getElementById("two");
-const threeButton = document.getElementById("three");
-const fourButton = document.getElementById("four");
-const fiveButton = document.getElementById("five");
-const sixButton = document.getElementById("six");
-const sevenButton = document.getElementById("seven");
-const eightButton = document.getElementById("eight");
-const nineButton = document.getElementById("nine");
+calculate = (oldValue, action, currentValue) => {
+   if (action === "add") {resultValue = oldValue + currentValue;} 
+   else if (action === "subtrack") {resultValue = oldValue - currentValue;} 
+   else if (action === "multiplication") {resultValue = oldValue * currentValue;} 
+   else if (action === "division") {resultValue = oldValue / currentValue;}
+   return resultValue;
+};
 
-let numbersButtons = [
-   zeroButton,
-   oneButton,
-   twoButton,
-   threeButton,
-   fourButton,
-   fiveButton,
-   sixButton,
-   sevenButton,
-   eightButton,
-   nineButton,
-];
+restartDisplay = () => {
+   result.innerText = "0";
+};
 
-numbersButtons.forEach((numberButton) => {
-   numberButton.addEventListener("click", () => {
-      if (Number(result.innerText) === 0) {
-         result.innerText = Number(numberButton.innerText);
-      } else {
-         result.innerText += Number(numberButton.innerText);
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+   if (button?.className === "operation-btn") {
+      if (button?.id === "division") {
+         button.addEventListener("click", () => {
+            action = "division";
+            oldValue = result.innerText;
+            restartDisplay();
+         });
+      } else if (button?.id === "multiplication") {
+         button.addEventListener("click", () => {
+            action = "multiplication";
+            oldValue = result.innerText;
+            restartDisplay();
+         });
+      } else if (button?.id === "subtrack") {
+         button.addEventListener("click", () => {
+            action = "subtrack";
+            oldValue = result.innerText;
+            restartDisplay();
+         });
+      } else if (button?.id === "add") {
+         button.addEventListener("click", () => {
+            action = "add";
+            oldValue = result.innerText;
+            restartDisplay();
+         });
+      } else if (button?.id === "equals") {
+         button.addEventListener("click", () => {
+            if (action && oldValue) {
+               let n1 = oldValue;
+               let n2 = result.innerText;
+               resultValue = calculate(n1, action, n2);
+               if (resultValue) {result.innerText = resultValue;}
+            }
+         });
       }
-   });
+   } else {
+      button.addEventListener("click", () => {
+         if (result.innerText === "0") {result.innerText = button.innerText;} 
+         else {result.innerText += button.innerText;}
+      });
+   }
 });
-
-const operationBtns = document.querySelectorAll(".operation-btn");
-
-addition = () => {};
-
-substraction = () => {};
-
-multiplication = () => {};
-
-divison = () => {};
