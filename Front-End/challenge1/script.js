55dc3f27c76e48eccbcc14d4b119fe7489ec4de7
Douglas Zúñiga <douglaszs397@hotmail.com>
2022-01-07 20:23:33 -0500

    Front-End challenge

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,15 @@
 // Modify this file only
+const counter = document.getElementById('counter');
+let counterValue = parseInt(document.getElementById('counter').innerText, 10);
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+
+increase.addEventListener('click', () => {
+  counterValue += 1;
+  counter.innerHTML = counterValue;
+});
+
+decrease.addEventListener('click', () => {
+  counterValue -= 1;
+  counter.innerHTML = counterValue;
+});
