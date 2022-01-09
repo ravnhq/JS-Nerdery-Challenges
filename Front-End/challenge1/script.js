c81ef3c12a515e348f94b3864901fc631edb78bb
Jean Pierre <jean.mejia.rev@protonmail.com>
2022-01-09 18:35:54 -0500

    Front-End Challenge 1: Counter is now shown

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,10 @@
-// Modify this file only
+var count = 0;
+
+document.getElementById("increase").onclick = function(){
+    count ++;
+    document.getElementById("counter").textContent = count;
+}
+document.getElementById("decrease").onclick = function(){
+    count --;
+    document.getElementById("counter").textContent = count;
+}
