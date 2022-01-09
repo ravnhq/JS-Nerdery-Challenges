62c270b1d89d4e1f7d035bfa231c59d10ec52a8a
Jean Pierre <jean.mejia.rev@protonmail.com>
2022-01-09 16:55:20 -0500

    Challenge 1

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -14,6 +14,21 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 
 const readableTime = (seconds) => {
   // YOUR CODE HERE...
+  let hours = Math.floor(seconds/3600);
+  let minutes = Math.floor((seconds - (hours * 3600))/60);
+  let secs = seconds - (hours * 3600) - (minutes * 60);
+  
+  if (hours < 10) {
+  	hours = "0" + hours;
+  }
+  if (minutes < 10) {
+  	minutes = "0" + minutes;
+  }
+  if (secs < 10) {
+  	secs = "0" + secs;
+  }
+  
+  return hours + ':' + minutes + ':' + secs;
 };
 
 readableTime(458);
