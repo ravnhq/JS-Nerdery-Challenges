83cd482672cc2b848aad01ab44c5fbd4a90b6f22
bllerenac <bllerenac@uni.pe>
2022-01-09 18:59:26 -0500

    create 3 variables for map the basic compents in the Counter

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,4 @@
 // Modify this file only
+const display_counter = document.getElementById("counter")
+const btn_increase = document.getElementById("increase")
+const btn_decrease = document.getElementById("decrease")
