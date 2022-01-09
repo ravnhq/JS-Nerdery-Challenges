89293b87fecad929b61131d5bab8956204e19c38
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-01-09 15:09:31 -0500

    feat: frontend challenge - calculator and counter finished

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,17 @@
 // Modify this file only
+
+//references
+const value = document.querySelector("#counter");
+const increase = document.querySelector("#increase");
+const decrease = document.querySelector("#decrease");
+
+//count is a global variable that will be modified by events
+let counter = parseInt(value.innerText, 10);
+
+increase.addEventListener("click", () => {
+  value.innerHTML = counter++;
+});
+
+decrease.addEventListener("click", () => {
+  value.innerHTML = counter--;
+});
--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,3 +5,84 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+
+//Numbers
+let firstNumber = "";
+let secondNumber = "";
+
+//Arithmetic operator
+let operator = "";
+let action = true;
+
+//Show value
+let showValue = document.querySelector("#display");
+
+//Buttons with numbers
+document.querySelector("#zero").addEventListener("click", setNumber);
+document.querySelector("#one").addEventListener("click", setNumber);
+document.querySelector("#two").addEventListener("click", setNumber);
+document.querySelector("#three").addEventListener("click", setNumber);
+document.querySelector("#four").addEventListener("click", setNumber);
+document.querySelector("#five").addEventListener("click", setNumber);
+document.querySelector("#six").addEventListener("click", setNumber);
+document.querySelector("#seven").addEventListener("click", setNumber);
+document.querySelector("#eight").addEventListener("click", setNumber);
+document.querySelector("#nine").addEventListener("click", setNumber);
+
+//Buttons with arithmetic operators
+document.querySelector("#add").addEventListener("click", setOperator);
+document.querySelector("#subtrack").addEventListener("click", setOperator);
+document
+  .querySelector("#multiplication")
+  .addEventListener("click", setOperator);
+document.querySelector("#division").addEventListener("click", setOperator);
+document.querySelector("#equals").addEventListener("click", setOperator);
+
+function setNumber() {
+  if (action) {
+    operator = "";
+    firstNumber += this.innerText;
+    showValue.innerText = firstNumber;
+  }
+  if (!action) {
+    secondNumber += this.innerText;
+    showValue.innerText = `${firstNumber} ${operator} ${secondNumber}`;
+  }
+}
+
+function setOperator() {
+  if (this.innerText === "=") {
+    action = true;
+    executeOperation();
+  }
+
+  if (operator === "") {
+    operator = this.innerText;
+    action = false;
+  }
+}
+
+function clearVariables() {
+  firstNumber = "";
+  secondNumber = "";
+}
+
+const executeOperation = () => {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = parseInt(firstNumber, 10) + parseInt(secondNumber, 10);
+      break;
+    case "-":
+      result = parseInt(firstNumber, 10) - parseInt(secondNumber, 10);
+      break;
+    case "X":
+      result = parseInt(firstNumber, 10) * parseInt(secondNumber, 10);
+      break;
+    case "/":
+      result = parseInt(firstNumber, 10) / parseInt(secondNumber, 10);
+      break;
+  }
+  showValue.innerText = result;
+  clearVariables();
+};
