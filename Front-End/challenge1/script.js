5bf0cca2d2979bb8a31a5fe5c4c27f430bfbf011
bllerenac <bllerenac@uni.pe>
2022-01-09 19:00:43 -0500

    add function in an increase to add 1 in the counter

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -2,3 +2,8 @@
 const display_counter = document.getElementById("counter")
 const btn_increase = document.getElementById("increase")
 const btn_decrease = document.getElementById("decrease")
+
+btn_increase.addEventListener( "click" , e => {
+  display_counter.textContent = parseInt(display_counter.textContent) + 1 
+  console.log(display_counter.textContent)
+})
