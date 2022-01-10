e3ae0e1a0efbc15cd8494394e354d793bb39c10b
Jean Pierre <jean.mejia.rev@protonmail.com>
2022-01-09 21:52:29 -0500

    deleted script from challenge 2

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,39 +5,5 @@ TO-DO:
 - The calculator should be completely functional
 */
 
-const calc = document.querySelector('.wrapper')
-const buttons = calc.querySelector('.row')
 
-
-buttons.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target
-        const action = key.dataset.action
-        
-        if (!action) {
-            console.log('number key!')
-        }
-        
-        if (
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-        ) {
-            console.log('operator key!')
-        }
-        if (action === 'decimal') {
-            console.log('decimal key!')
-        }
-        
-        if (action === 'clear') {
-            console.log('clear key!')
-        }
-        
-        if (action === 'calculate') {
-            console.log('equal key!')
-        }
-        
-    }
-})
 
