bdc26d4fbbdf191aea8f6808b8723e78a56efbc2
Jean Pierre <jean.mejia.rev@protonmail.com>
2022-01-21 11:32:04 -0500

    feat!: Challenges are now in TS, add .json required file

--- JS-Algorithms/challenges.ts
+++ JS-Algorithms/challenges.ts
@@ -12,8 +12,31 @@ Example:
 Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 ***** */
 
-const readableTime = (seconds) => {
-  // YOUR CODE HERE...
+const readableTime = (seconds: number): string => {
+  let hours: number = Math.floor(seconds / 3600),
+    minutes: number = Math.floor((seconds - hours * 3600) / 60),
+    secs: number = seconds - hours * 3600 - minutes * 60;
+
+  let strHours: string = "",
+    strMinutes: string = "",
+    strSecs: string = "";
+  //Get hours
+  if (hours < 10) {
+    strHours = "0" + hours;
+  }
+  //Get minutes
+  if (minutes < 10) {
+    strMinutes = "0" + minutes;
+  }
+  //Get seconds
+  if (secs < 10) {
+    strSecs = "0" + secs;
+  } else {
+    strSecs = secs.toString();
+  }
+  //Returns a readable time
+  console.log(secs);
+  return strHours + ":" + strMinutes + ":" + strSecs;
 };
 
 readableTime(458);
@@ -40,8 +63,14 @@ Invoking "circularArray(2)" should return "["Island", "Japan", "Israel", "German
 
 const COUNTRY_NAMES = ["Germany", "Norway", "Island", "Japan", "Israel"];
 
-const circularArray = (index) => {
+const circularArray = (index: number): string[] => {
   // YOUR CODE HERE...
+  const len = COUNTRY_NAMES.length;
+  const newIndex = ((index % len) + len) % len;
+  let newArray = COUNTRY_NAMES.slice(newIndex, len);
+  newArray = newArray.concat(COUNTRY_NAMES.slice(0, newIndex));
+
+  return newArray;
 };
 
 circularArray(2);
@@ -69,8 +98,15 @@ because 1^1 + 2^2 + 3^3 + 4^4 + 5^5 + 6^6 + 7^7 + 8^8 + 9^9 + 10^10 = 1040507131
 The last 3 digits for the sum of powers from 1 to 10 is "317"
 ***** */
 
-const ownPower = (number, lastDigits) => {
+const ownPower = (number: number, lastDigits: number): string => {
   // YOUR CODE HERE...
+  let num = 0;
+
+  for (let n = 1; n < number + 1; n++) {
+    num = num + Math.pow(n, n);
+  }
+  let bigNum = BigInt(num);
+  return bigNum.toString().slice(-lastDigits);
 };
 
 ownPower(10, 3);
@@ -94,8 +130,23 @@ Invoking "digitSum(10)" should return "27".
 Since 10! === 3628800 and you sum 3 + 6 + 2 + 8 + 8 + 0 + 0
 ***** */
 
-const digitSum = (n) => {
+const digitSum = (n: number): number => {
   // YOUR CODE HERE...
+  let result = n,
+    finalSum = 0;
+
+  while (n > 1) {
+    n--;
+    result = result * n;
+  }
+
+  let fact = String(BigInt(result));
+
+  for (let i = 0; i < fact.length; i++) {
+    finalSum += Number(fact[i]);
+  }
+
+  return finalSum;
 };
 
 digitSum(10);
@@ -117,8 +168,24 @@ Invoking "fibIndex(3)" should return "12".
 Because the 12th index in the Fibonacci sequence is 144, and 144 has three digits
 ***** */
 
-const fibIndex = (n) => {
+const fibIndex = (n: number): number => {
   // YOUR CODE HERE...
+  let n1 = 0,
+    n2 = 1,
+    next,
+    cont = 0;
+  next = n1 + n2;
+
+  while (next) {
+    cont = cont + 1;
+    if (next.toString().length === n) {
+      return cont + 1;
+    }
+    n1 = n2;
+    n2 = next;
+    next = n1 + n2;
+  }
+  return 1;
 };
 
 fibIndex(3);
