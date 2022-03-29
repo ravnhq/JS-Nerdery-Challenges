fcdb44da9741b98d36b6d0f7b3a0af9b374e1dbc
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-03-28 21:58:22 -0600

    initial commit: add solution to challenge one

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,26 @@
 // Modify this file only
+const app = document.querySelector('body');
+const counterDisplay = document.getElementById('counter');
+const decreaseButton = document.getElementById('decrease');
+const increaseButton = document.getElementById('increase');
+let count = parseInt(counterDisplay.innerText);
+
+const increaseCount = () => {
+  count += 1;
+  counterDisplay.innerText = count;
+};
+
+const decreaseCount = () => {
+  if (count > 0) {
+    count -= 1;
+  }
+  counterDisplay.innerText = count;
+};
+
+app.addEventListener('click', (e) => {
+  if (e.target === decreaseButton) {
+    decreaseCount();
+  } else if (e.target === increaseButton) {
+    increaseCount();
+  }
+});
