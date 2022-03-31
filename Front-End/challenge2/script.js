e32d3454fb52f36812d6999d2ddb25f082f4bf9a
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-03-31 12:28:45 -0600

    add solution for "Front-end"/challenge2

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -4,6 +4,12 @@ TO-DO:
 - The calculator should be completely functional
 */
 
+/*
+How to use:
+(C) key or Clear: Since there is no clear button, the equals operator serves
+as clear instead of its usual repeat last operation functionality.
+*/
+
 const calculatorApp = document.querySelectorAll('button');
 const display = document.getElementById('display');
 let result = '';
@@ -14,46 +20,57 @@ const appendDigitToOperand = (button) => {
   display.innerText = eval(operand);
 };
 
-const updateResult = () => {
+const updateDisplay = () => {
   display.innerText = (eval(result.slice(0, result.length - 1)));
 };
 
-const displayFinalResult = () => {
-  result += operand;
-  if (!isFinite(eval(result))) {
-    display.innerText = 'Error';
-  } else {
-    display.innerText = eval(result);
-  }
+const updateResult = (button) => {
+  result += 0 + operand + button;
+};
+
+const clearResult = () => {
   operand = '0';
   result = '';
+};
+
+const clearOperand = () => {
+  operand = '';
+};
+
+const displayFinalResult = () => {
+  try {
+    if (!isFinite(eval(result))) {
+      display.innerText = 'Error';
+    } else {
+      display.innerText = eval(result);
+    }
+  } catch (error) {
+    updateDisplay();
+  }
+  clearResult();
 };
 
 calculatorApp.forEach((x) => x.addEventListener('click', (e) => {
   const button = e.target.innerText;
   switch (button) {
     case '=':
+      result += operand;
       displayFinalResult();
       break;
     case '+':
     case '-':
-      result += operand + button;
-      updateResult();
-      operand = '';
-      break;
     case '/':
-      result += 0 + operand + button;
-      operand = '';
+      updateResult(button);
+      updateDisplay();
+      clearOperand();
       break;
     case 'X':
-      result += `${0} ${operand}*`;
-      operand = '';
+      result += `0${operand}*`;
+      updateDisplay();
+      clearOperand();
       break;
     default:
       appendDigitToOperand(button);
       break;
   }
 }));
-
-// fix: equals after operator
-// todo: function composition to replace switch execution statements
