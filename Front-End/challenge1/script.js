47663d47f02db8427723ef168149306202ee6989
Gabriel Nima <gabrielnima@ravn.co>
2022-04-06 11:53:59 -0500

    challenge completed

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,14 @@
-// Modify this file only
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const span = document.getElementById("counter");
+let i = 0;
+
+increase.addEventListener("click", function (e) {
+    i++;
+    span.innerHTML = i;
+});
+
+decrease.addEventListener("click", function (e) {
+    i--;
+    span.innerHTML = i;
+});
--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,3 +5,82 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const display = document.getElementById("display");
+const add = document.getElementById('add')
+const equals = document.getElementById('equals')
+const subtrack = document.getElementById('subtrack')
+const multiplication = document.getElementById('multiplication')
+const division = document.getElementById('division')
+let touch = 0;
+let result = 0;
+
+function evaluate(fn) {
+  return new Function('return ' + fn)();
+}
+
+function addValue(value) {
+  if (touch == 0) {
+    if (value == 0) {
+      return
+    } else {
+      display.innerHTML = value;
+      touch++;
+    }
+  } else {
+    if (value == 0 && display.textContent == "0") {
+      return
+    } else {
+      display.innerHTML += value;
+    }
+  }
+
+}
+
+function addSymbol(value) {
+  if (touch == 0) {
+    return
+  } else {
+    if (display.textContent[display.textContent.length - 1] == value || !parseInt(display.textContent[display.textContent.length - 1]) ) {
+      return
+    } else {
+      display.innerHTML += value;
+    }
+
+  }
+}
+
+function resolve() {
+  result = display.textContent
+  result = result.split('').map((element, index) => element == "X" ? result[index] = "*" : element).toString().replace(/,/g, "")
+  console.log(result)
+  display.innerHTML = evaluate(result)
+  console.log(result)
+  touch = 0;
+}
+
+
+zero.addEventListener("click", () => addValue(zero.textContent));
+one.addEventListener("click", () => addValue(one.textContent));
+two.addEventListener("click", () => addValue(two.textContent));
+three.addEventListener("click", () => addValue(three.textContent));
+four.addEventListener("click", () => addValue(four.textContent));
+five.addEventListener("click", () => addValue(five.textContent));
+six.addEventListener("click", () => addValue(six.textContent));
+seven.addEventListener("click", () => addValue(seven.textContent));
+eight.addEventListener("click", () => addValue(eight.textContent));
+nine.addEventListener("click", () => addValue(nine.textContent));
+add.addEventListener("click", () => addSymbol(add.textContent));
+multiplication.addEventListener("click", () => addSymbol(multiplication.textContent));
+division.addEventListener("click", () => addSymbol(division.textContent));
+subtrack.addEventListener("click", () => addSymbol(subtrack.textContent));
+equals.addEventListener("click", () => resolve());
