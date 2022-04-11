da9db15a725cd839a23c68f626769e5092a29ec4
VanessaMMH <mahua.ea@gmail.com>
2022-04-11 10:43:18 -0500

    Development challenges

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,28 @@
 // Modify this file only
+'use strict'
+
+let counter = 0;
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+
+
+
+const Increase = () => {
+    updateCounter(++counter);
+}
+const Decrease = () => {
+    updateCounter(--counter);
+}
+const updateCounter = (value) => {
+    document.getElementById('counter').textContent = value;
+}
+
+increase.addEventListener('click', () => {
+    Increase()
+})
+
+decrease.addEventListener('click', () => {
+    Decrease()
+})
+
+
--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,3 +5,104 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+
+"use strict"
+
+const wrapper = document.querySelector('.wrapper')
+
+let numberStr = ''
+let viewString = ''
+let operations = ['+', '-', 'X', '/']
+let stack = []
+
+const updateValue = (value) => {
+    document.getElementById('display').textContent = value;
+}
+
+const evaluateStack = (arrStack) => {
+
+    //Convertimos a Notacion Posfija
+    if (arrStack.length == 0)
+        return 0;
+
+    let auxStack = []
+    let lastOp = '$';
+    for (let i = 0; i < arrStack.length; i++) {
+        if (operations.includes(arrStack[i])) {
+            lastOp = arrStack[i];
+        } else {
+            auxStack.push(arrStack[i])
+            if (lastOp != '$') {
+                auxStack.push(lastOp)
+            }
+        }
+    }
+    console.log(auxStack)
+    
+
+
+    let mainStack = []
+    let last;
+    mainStack.push(parseFloat(auxStack[0]));
+
+    for (let i = 1; i < auxStack.length; i++) {
+        let current = auxStack[i]
+        if (!operations.includes(current)) {
+            mainStack.push(current)
+        } else {
+            switch (current) {
+                case '+':
+                    last = mainStack.pop()
+                    mainStack[mainStack.length - 1] += parseFloat(last);
+                    break;
+                case '-':
+                    last = mainStack.pop()
+                    mainStack[mainStack.length - 1] -= parseFloat(last);
+                    break;
+                case '/':
+                    last = mainStack.pop()
+                    mainStack[mainStack.length - 1] /= parseFloat(last);
+                    break;
+                case 'X':
+                    last = mainStack.pop()
+                    mainStack[mainStack.length - 1] *= parseFloat(last);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return mainStack[0]
+}
+
+
+
+wrapper.addEventListener('click', (e1) => {
+    let val = e1.target.textContent
+    console.log(val)
+    if (operations.includes(val)) {
+        stack.push(numberStr);
+        stack.push(val)
+        numberStr = ''
+        console.log(stack)
+        viewString += val
+        updateValue(viewString)
+    }
+    else if (val === '=') {
+        console.log('igual')
+        stack.push(numberStr);
+        numberStr = ''
+        console.log(stack)
+        updateValue(evaluateStack(stack));
+        stack = []
+        viewString = ''
+    }
+    else {
+        numberStr += val;
+        viewString += val
+        updateValue(viewString)
+
+    }
+
+
+})
