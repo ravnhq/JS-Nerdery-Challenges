14d40a8cf02eba5edf68f89752e9bf10fd9e479b
Freddy-1703 <freddy.bedregal.t@gmail.com>
2022-04-11 09:41:28 -0500

    Algorithms Challenge solved

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -14,6 +14,16 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 
 const readableTime = (seconds) => {
   // YOUR CODE HERE...
+  let hours = Math.floor(seconds / 3600);
+  seconds = seconds % 3600;
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+
+  if(hours < 10) { hours = "0" + hours;}
+  if(minutes < 10) {minutes = "0" + minutes;}
+  if(seconds < 10) {seconds = "0" + seconds;}
+
+  return `${hours}:${minutes}:${seconds}`;
 };
 
 readableTime(458);
@@ -39,9 +49,23 @@ Invoking "circularArray(2)" should return "["Island", "Japan", "Israel", "German
 ***** */
 
 const COUNTRY_NAMES = ["Germany", "Norway", "Island", "Japan", "Israel"];
+const MODIFIED_COUNTRY_NAMES = new Array();
 
 const circularArray = (index) => {
   // YOUR CODE HERE...
+  if(index >= 5) index = index % 5;
+
+  for(let countryIndex = 0; countryIndex < COUNTRY_NAMES.length; countryIndex++){
+
+    MODIFIED_COUNTRY_NAMES[countryIndex] = COUNTRY_NAMES[index];
+    index++;
+
+    if(index >= COUNTRY_NAMES.length){
+      index = 0;
+    }
+  }
+
+  return MODIFIED_COUNTRY_NAMES; 
 };
 
 circularArray(2);
@@ -71,6 +95,30 @@ The last 3 digits for the sum of powers from 1 to 10 is "317"
 
 const ownPower = (number, lastDigits) => {
   // YOUR CODE HERE...
+  let sumPow = 0;
+  let numPow = 1;
+
+  for(let evaluatedNum = 1; evaluatedNum <= number; evaluatedNum++){
+
+    numPow = 1;
+
+    for(let index = 0; index < evaluatedNum; index++){
+      numPow = numPow * evaluatedNum;
+    }
+
+    sumPow = sumPow + numPow;
+  }
+
+  sumPow = BigInt(sumPow);
+
+  let sumInString = sumPow.toString();
+  let finalString = "";
+
+  for(let finalDigit = lastDigits; finalDigit > 0 ; finalDigit--){
+    finalString = finalString + sumInString[sumInString.length - finalDigit];
+  }
+
+  return finalString;
 };
 
 ownPower(10, 3);
@@ -96,6 +144,21 @@ Since 10! === 3628800 and you sum 3 + 6 + 2 + 8 + 8 + 0 + 0
 
 const digitSum = (n) => {
   // YOUR CODE HERE...
+  let factNum = 1;
+
+  for(let index = 0; index < n; index++){
+    factNum = factNum * (n - index);
+  }
+
+  factNum = BigInt(factNum);
+  let numberInString = String(factNum);
+  let sumDigits = 0;
+
+  for(let index = 0; index < numberInString.length; index++){
+    sumDigits = sumDigits + Number(numberInString[index]);
+  }
+
+  return sumDigits;
 };
 
 digitSum(10);
@@ -119,6 +182,27 @@ Because the 12th index in the Fibonacci sequence is 144, and 144 has three digit
 
 const fibIndex = (n) => {
   // YOUR CODE HERE...
+  let fibonacciList = new Array();
+  let firstNum = 0;
+  let secondNum = 1;
+  let sumFirstAndSec = firstNum + secondNum;
+  let indexFirstFib = 0;
+
+  fibonacciList.push(firstNum);
+  fibonacciList.push(secondNum);
+  fibonacciList.push(sumFirstAndSec);
+
+  while(sumFirstAndSec.toString().length < (n + 1)){
+    firstNum = secondNum;
+    secondNum = sumFirstAndSec;
+    sumFirstAndSec = firstNum + secondNum;
+    fibonacciList.push(sumFirstAndSec);
+
+    if(sumFirstAndSec.toString().length === n){
+      indexFirstFib = fibonacciList.indexOf(sumFirstAndSec);
+      return indexFirstFib;
+    }
+  }
 };
 
 fibIndex(3);
