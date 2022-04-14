f625f6bd5847df7519ce8721fac6fbc6b3d268c1
Gabriel Nima <gabrielnima@ravn.co>
2022-04-13 23:54:19 -0500

    refactoring: suggestions implemented in challenge 2

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,97 +5,98 @@ TO-DO:
 - The calculator should be completely functional
 
 */
-const zero = document.getElementById("zero");
-const one = document.getElementById("one");
-const two = document.getElementById("two");
-const three = document.getElementById("three");
-const four = document.getElementById("four");
-const five = document.getElementById("five");
-const six = document.getElementById("six");
-const seven = document.getElementById("seven");
-const eight = document.getElementById("eight");
-const nine = document.getElementById("nine");
-const display = document.getElementById("display");
-const add = document.getElementById('add')
-const equals = document.getElementById('equals')
-const subtrack = document.getElementById('subtrack')
-const multiplication = document.getElementById('multiplication')
-const division = document.getElementById('division')
-let touch = 0;
+const arrayButton = document.querySelectorAll("button");
+const arrayOperation = [];
+const arrayNumbers = [];
+let isTouched = false;
 let result = 0;
-let flag = false;
+let isResolved = false;
 
-function evaluate(fn) {
-  return new Function('return ' + fn)();
+for (let button of arrayButton) {
+  if (
+    button.textContent !== "/" &&
+    button.textContent !== "-" &&
+    button.textContent !== "+" &&
+    button.textContent !== "X" &&
+    button.textContent !== "="
+  ) {
+    arrayNumbers.push(button);
+  } else {
+    arrayOperation.push(button);
+  }
+}
+
+function evaluate(mathematicExpression) {
+  return new Function("return " + mathematicExpression)();
 }
 
 function addValue(value) {
-  if (touch == 0) {
+  if (isTouched === false) {
     display.innerHTML = value;
-    touch = 1
+    isTouched = true;
   } else {
-    if (value == "0" && display.textContent == "0") {
-      return
+    if (value === "0" && display.textContent === "0") {
+      return;
     } else {
-      if (display.textContent == "0") {
-        display.innerHTML = value
+      if (display.textContent === "0") {
+        display.innerHTML = value;
       } else {
         display.innerHTML += value;
       }
-
     }
   }
-
 }
 
 function addSymbol(value) {
-  if (touch == 0 && flag == false) {
-    return
-  } else {
-    touch = 1
-    if (display.textContent[display.textContent.length - 1] == value || !parseInt(display.textContent[display.textContent.length - 1])) {
+  const notConsecutiveOperators =
+    display.textContent[display.textContent.length - 1] == value ||
+    !parseInt(display.textContent[display.textContent.length - 1]);
 
+  if (isTouched === false && isResolved === false) {
+    return;
+  } else {
+    isTouched = true;
+    if (notConsecutiveOperators) {
       if (display.textContent[display.textContent.length - 1] == "0") {
         display.innerHTML += value;
-
       }
-      return
+      return;
     } else {
       display.innerHTML += value;
-
     }
-
   }
 }
 
 function resolve() {
-  result = display.textContent
-  result = result.split('').map((element, index) => element == "X" ? result[index] = "*" : element).toString().replace(/,/g, "")
+  result = display.textContent;
+  result = result
+    .split("")
+    .map((element, index) => (element == "X" ? (result[index] = "*") : element))
+    .toString()
+    .replace(/,/g, "");
   try {
-    display.innerHTML = evaluate(result)
-    touch = 0
-    flag = true
+    display.innerHTML = evaluate(result);
+    isTouched = false;
+    isResolved = true;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  return true
-
-
 }
 
+for (let i = 0; i < arrayNumbers.length; i++) {
+  arrayNumbers[i].addEventListener("click", () => {
+    addValue(arrayNumbers[i].textContent);
+  });
+}
 
-zero.addEventListener("click", () => addValue(zero.textContent));
-one.addEventListener("click", () => addValue(one.textContent));
-two.addEventListener("click", () => addValue(two.textContent));
-three.addEventListener("click", () => addValue(three.textContent));
-four.addEventListener("click", () => addValue(four.textContent));
-five.addEventListener("click", () => addValue(five.textContent));
-six.addEventListener("click", () => addValue(six.textContent));
-seven.addEventListener("click", () => addValue(seven.textContent));
-eight.addEventListener("click", () => addValue(eight.textContent));
-nine.addEventListener("click", () => addValue(nine.textContent));
-add.addEventListener("click", () => addSymbol(add.textContent));
-multiplication.addEventListener("click", () => addSymbol(multiplication.textContent));
-division.addEventListener("click", () => addSymbol(division.textContent));
-subtrack.addEventListener("click", () => addSymbol(subtrack.textContent));
-equals.addEventListener("click", () => resolve());
+for (let i = 0; i < arrayOperation.length; i++) {
+  if (arrayOperation[i].textContent === "=") {
+    arrayOperation[i].addEventListener("click", () => {
+      resolve();
+    });
+  } else {
+    arrayOperation[i].addEventListener("click", () => {
+      addSymbol(arrayOperation[i].textContent);
+    });
+  }
+}
