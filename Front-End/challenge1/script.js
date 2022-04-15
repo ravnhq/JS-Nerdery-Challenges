2bba392c1b239f165e63855a0871670964f0ffad
Freddy-1703 <freddy.bedregal.t@gmail.com>
2022-04-15 08:55:20 -0500

    Changed let instead var // Way of autoincrement variable

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,15 +1,15 @@
 // Modify this file only
-var actualNumber = 0;
+let actualNumber = 0;
 
 document.getElementById("increase").addEventListener("click", increaseNumber);
 document.getElementById("decrease").addEventListener("click", decreaseNumber);
 
 function increaseNumber(){
-    actualNumber = actualNumber + 1;
+    actualNumber += 1;
     document.getElementById("counter").innerHTML = actualNumber;
 }
 
 function decreaseNumber(){
-    actualNumber = actualNumber - 1;
+    actualNumber -= 1;
     document.getElementById("counter").innerHTML = actualNumber;
 }
