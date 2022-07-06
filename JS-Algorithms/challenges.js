e9831e929e76cb5e2f7e3517f3edd3b8693af7c5
Kevin Ingles <kfabian@applaudostudios.dev>
2022-07-06 09:25:55 -0600

    ✨complete nerdery challenges/challenge1

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -14,6 +14,18 @@ Invoking "readableTime(3690)" should return "01:01:30" (HH:MM:SS)
 
 const readableTime = (seconds) => {
   // YOUR CODE HERE...
+
+  let hours = Math.floor(seconds / 3600);
+
+  const totalSeconds = seconds % 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let timeSeconds = totalSeconds % 60;
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  timeSeconds = timeSeconds < 10 ? '0' + timeSeconds : timeSeconds;
+
+  const convertedTime = `${hours}:${minutes}:${timeSeconds}`;
+  return convertedTime;
 };
 
 readableTime(458);
@@ -38,7 +50,7 @@ Example:
 Invoking "circularArray(2)" should return "["Island", "Japan", "Israel", "Germany", "Norway"]"
 ***** */
 
-const COUNTRY_NAMES = ["Germany", "Norway", "Island", "Japan", "Israel"];
+const COUNTRY_NAMES = ['Germany', 'Norway', 'Island', 'Japan', 'Israel'];
 
 const circularArray = (index) => {
   // YOUR CODE HERE...
@@ -49,85 +61,85 @@ circularArray(3);
 circularArray(5);
 circularArray(9);
 
-/* *****
-Challenge 3
+// /* *****
+// Challenge 3
 
-"Own Powers"
+// "Own Powers"
 
-The function "ownPower" accepts two arguments. "number" and "lastDigits".
+// The function "ownPower" accepts two arguments. "number" and "lastDigits".
 
-The "number" indicates how long is the series of numbers you are going to work with, your
-job is to multiply each of those numbers by their own powers and after that sum all the results.
+// The "number" indicates how long is the series of numbers you are going to work with, your
+// job is to multiply each of those numbers by their own powers and after that sum all the results.
 
-"lastDigits" is the length of the number that your function should return, as a string!.
-See example below.
+// "lastDigits" is the length of the number that your function should return, as a string!.
+// See example below.
 
-Example:
+// Example:
 
-Invoking "ownPower(10, 3)" should return "317"
-because 1^1 + 2^2 + 3^3 + 4^4 + 5^5 + 6^6 + 7^7 + 8^8 + 9^9 + 10^10 = 10405071317
-The last 3 digits for the sum of powers from 1 to 10 is "317"
-***** */
+// Invoking "ownPower(10, 3)" should return "317"
+// because 1^1 + 2^2 + 3^3 + 4^4 + 5^5 + 6^6 + 7^7 + 8^8 + 9^9 + 10^10 = 10405071317
+// The last 3 digits for the sum of powers from 1 to 10 is "317"
+// ***** */
 
-const ownPower = (number, lastDigits) => {
-  // YOUR CODE HERE...
-};
+// const ownPower = (number, lastDigits) => {
+//   // YOUR CODE HERE...
+// };
 
-ownPower(10, 3);
-ownPower(12, 7);
-ownPower(21, 12);
+// ownPower(10, 3);
+// ownPower(12, 7);
+// ownPower(21, 12);
 
-/* *****
-Challenge 4
+// /* *****
+// Challenge 4
 
-"Sum of factorial digits"
+// "Sum of factorial digits"
 
-A factorial (x!) means x! * (x - 1)... * 3 * 2 * 1.
-For example: 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800
+// A factorial (x!) means x! * (x - 1)... * 3 * 2 * 1.
+// For example: 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800
 
-Modify the function "digitSum" to return a number that
-equals to the sum of the digits in the result of 10!
+// Modify the function "digitSum" to return a number that
+// equals to the sum of the digits in the result of 10!
 
-Example:
+// Example:
 
-Invoking "digitSum(10)" should return "27".
-Since 10! === 3628800 and you sum 3 + 6 + 2 + 8 + 8 + 0 + 0
-***** */
+// Invoking "digitSum(10)" should return "27".
+// Since 10! === 3628800 and you sum 3 + 6 + 2 + 8 + 8 + 0 + 0
+// ***** */
 
-const digitSum = (n) => {
-  // YOUR CODE HERE...
-};
+// const digitSum = (n) => {
+//   // YOUR CODE HERE...
+// };
 
-digitSum(10);
-digitSum(42);
-digitSum(71);
-digitSum(89);
+// digitSum(10);
+// digitSum(42);
+// digitSum(71);
+// digitSum(89);
 
-/* *****
-Challenge 5
+// /* *****
+// Challenge 5
 
-"N-Digit Fibonacci Number"
+// "N-Digit Fibonacci Number"
 
-Modify the function "fibIndex" to return the index of the first Fibonacci
-number whose digits-length equals the number passed in to the function.
+// Modify the function "fibIndex" to return the index of the first Fibonacci
+// number whose digits-length equals the number passed in to the function.
 
-Example:
+// Example:
 
-Invoking "fibIndex(3)" should return "12".
-Because the 12th index in the Fibonacci sequence is 144, and 144 has three digits
-***** */
+// Invoking "fibIndex(3)" should return "12".
+// Because the 12th index in the Fibonacci sequence is 144, and 144 has three digits
+// ***** */
 
-const fibIndex = (n) => {
-  // YOUR CODE HERE...
-};
+// const fibIndex = (n) => {
+//   // YOUR CODE HERE...
+// };
 
-fibIndex(3);
-fibIndex(5);
-fibIndex(12);
-fibIndex(15);
+// fibIndex(3);
+// fibIndex(5);
+// fibIndex(12);
+// fibIndex(15);
 
-exports.readableTime = readableTime;
-exports.circularArray = circularArray;
-exports.ownPower = ownPower;
-exports.digitSum = digitSum;
-exports.fibIndex = fibIndex;
+// exports.readableTime = readableTime;
+// exports.circularArray = circularArray;
+// exports.ownPower = ownPower;
+// exports.digitSum = digitSum;
+// exports.fibIndex = fibIndex;
