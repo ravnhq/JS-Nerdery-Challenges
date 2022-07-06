fd5dbf631f0eff9277037686529e75ac20f4a340
Kevin Ingles <kfabian@applaudostudios.dev>
2022-07-06 16:11:14 -0600

    ✨ Add increase feature

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,9 @@
 // Modify this file only
+const buttonIncrease = document.querySelector('#increase');
+const buttonDecrease = document.querySelector('#decrease');
+const counterSpan = document.querySelector('#counter');
+let i = 0;
+buttonIncrease.addEventListener('click', (event) => {
+  i++;
+  counterSpan.innerHTML = i;
+});
