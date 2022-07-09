1a68da901a24303c9ffd763fd4dd612ce41066fc
gerardotamo <gerardotamo@ravn.co>
2022-07-09 11:53:11 -0500

    Fron-end challenge2, data entry for the operation

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -1,6 +1,7 @@
 const allButtons = document.querySelectorAll("button");
 const displayOperation = document.getElementById("display");
-
+let restart = true;
+let limitOperation = 0;
 const isSymbol = (symbol) => {
     switch (symbol) {
         case "/":
@@ -21,8 +22,31 @@ const isSymbol = (symbol) => {
 allButtons.forEach(button =>{
     button.onclick = () =>{
         if (isSymbol(button.innerText)) {
-            return console.log("Soy una operacion");
+            //Add symbols
+            if (limitOperation === 0) {
+                displayOperation.textContent = displayOperation.textContent + button.innerHTML;
+                return limitOperation = 1;
+            }
+            if (limitOperation === 2) {
+                return console.log("Realizar Operacion");
+            }
         }
-        return console.log("Soy un numero");
+
+        //Add numbers
+        if (restart) {
+            if (button.innerText === "0") {
+                return;
+            }
+            displayOperation.textContent = button.innerHTML;
+            return restart = false;
+        }
+        if (limitOperation === 1) {
+            if (button.innerText === "0") {
+                return;
+            }
+            limitOperation = 2;
+        }
+        displayOperation.textContent = displayOperation.textContent + button.innerHTML;
+             
     }
 })
