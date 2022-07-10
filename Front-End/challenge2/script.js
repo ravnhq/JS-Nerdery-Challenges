21449b83c727d8476cbfff06742c38339ccce5c0
Diana Calderon <dianacalderon@ravn.co>
2022-07-10 10:09:31 -0600

    Setting variables

--- Front-End/challenge2/script.js
+++ Front-End/challenge2/script.js
@@ -5,3 +5,24 @@ TO-DO:
 - The calculator should be completely functional
 
 */
+let a;
+let b;
+let operation;
+let total = 0;
+
+const display = document.getElementById("display");
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const add = document.getElementById("add");
+const subtrack = document.getElementById("subtrack");
+const multiplication = document.getElementById("multiplication");
+const division = document.getElementById("division");
+const equals = document.getElementById("equals");
